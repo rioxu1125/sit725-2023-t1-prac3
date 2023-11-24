c8bac0a48e41f83218a52fbdfc4e8477f44b2831
rioxu1125 <150398843+rioxu1125@users.noreply.github.com>
2023-11-24 11:38:42 +1100

    Prac3 completed

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express")
+var app = express()
+ 
+app.use(express.static(__dirname+'/public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+ 
+ 
+var port = process.env.port || 3000;
+ 
+ 
+ 
+ 
+app.listen(port, ()=>{
+    console.log("app listening to:" +port);
+})
